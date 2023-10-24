bfed5b6edd67e4c98968c06bd853d8f2b8338476
Bhargavi Borgohain <bhargaviborgohain@gmail.com>
2023-10-25 00:07:03 +0530

    initial CRUD backend

---
+++ backend/models/ToDoModel.js
@@ -0,0 +1,10 @@
+const { default: mongoose } = require("mongoose");
+
+const todoSchema = mongoose.Schema({
+  text: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("ToDo", todoSchema);
