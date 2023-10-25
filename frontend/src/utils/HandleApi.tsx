d813e6625a3d21e797b6aad56df191fc92256b9c
Bhargavi Borgohain <bhargaviborgohain@gmail.com>
2023-10-26 01:41:29 +0530

    frontend: display and add feature

---
+++ frontend/src/utils/HandleApi.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:5000";
+
+const getAllToDo = (setToDo) => {
+  axios.get(baseUrl).then(({ data }) => {
+    console.log("data ---> ", data);
+    setToDo(data);
+  });
+};
+
+const addToDo = (text, setText, setToDo) => {
+  axios.post(`${baseUrl}/save`, { text }).then((data) => {
+    console.log(data);
+    setText("");
+    getAllToDo(setToDo);
+  });
+};
+export { getAllToDo, addToDo };
